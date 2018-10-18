7d4d449f3ddea1723e01bb03f2091d26586ff389
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-10-18 09:36:10 -0400

    creates the struct KeyValue

--- Key-ValueStrore/KeyValue.cs
+++ Key-ValueStrore/KeyValue.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace Key_ValueStrore
 {
-    class Program
+    struct KeyValue
     {
-        static void Main(string[] args)
-        {
-           
-        }
+
     }
 }
